ffd552468ab3bd4efe2ab465bcdaedbcdcf81d41
Adam Prime <adam.prime@utoronto.ca>
2008-06-30 00:49:13 -0400

    removed some extra crap from the end of the file

--- lib/Apache2/DirBasedHandler.pm
+++ lib/Apache2/DirBasedHandler.pm
@@ -245,6 +245,3 @@ This module is distributed in the hope that it will be useful, but
 WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
 or FITNESS FOR A PARTICULAR PURPOSE.
 
-=cut
-
-1;
